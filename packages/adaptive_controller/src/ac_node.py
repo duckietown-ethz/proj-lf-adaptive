d229c47ea1b1d8d7b5378d9d2eda8dad4946a964
PietroGriffa <pietro.griffa@mail.polimi.it>
2019-12-07 16:04:13 +0100

    revised ac node

--- packages/adaptive_controller/src/ac_node.py
+++ packages/adaptive_controller/src/ac_node.py
@@ -39,11 +39,22 @@ class AdaptiveControllerNode(DTROS):
         #self.veh_name = rospy.get_param('~robot_name') #another method
         self.veh_name = rospy.get_namespace().strip("/")
 
-        # Get value for costant gamma
-        #self.gamma = rospy.get_param("~gamma") first like that
-        self.gamma = rospy.get_param("~gamma")
+        # V2: remove as much as possible all external params and replace them with self variables
+        self.gamma = 1
 
-        self.initialized = False
+        self.yp_k = np.asarray([0.0 0.0])
+        self.ym_k = self.yp_k
+        self.t_k = rospy.Time.now()
+        self.theta_hat_k = 0.0
+        self.ref_k = np.asarray([0.0 0.0])
+
+        self.yp_k_minus = self.yp_k
+        self.t_k_minus = self.t_k
+
+        # MAKE IT CLEANER: queste variabili non servono necessariamente, se non per tenere traccia dell'evoluzione
+        #self.theta_hat_k_minus = self.theta_hat_k
+        #self.ref_k_minus = self.ref_k
+
         # Publication
         self.pub_corrected_car_cmd = rospy.Publisher("~car_cmd", Twist2DStamped, queue_size=1)
 
@@ -58,12 +69,9 @@ class AdaptiveControllerNode(DTROS):
     def getPose(self, poseMsg):
         d = poseMsg.d
         phi = poseMsg.phi
-        time =  poseMsg.header.stamp
-        yp_lastPose = np.asarray([d, phi, time])
 
-        rospy.set_param("~yp_k", yp_lastPose)
-
-        #self.yp_lastPose = np.asarray([d, phi, time])
+        self.yp_k = np.asarray([d, phi])
+        self.t_k = poseMsg.header.stamp
 
 
     def correctCommand(self, car_cmd):
@@ -78,88 +86,31 @@ class AdaptiveControllerNode(DTROS):
 
         # start_time = rospy.Time.now()
 
-        if self.initialized == False:
-            yp_pres = rospy.get_param("~yp_lastPose")
-            yp_k = yp_pres[0:2]
-            t_k = yp_pres[2]
-            theta_hat_initialization_value = 0.0
-            v = 0.0
-            omega = 0.0
-            car_cmd_msg.v = v
-            car_cmd_msg.omega = omega
+        Ts = self.t_k - self.t_k_minus
 
-            # self.initialized = True
+        self.ym_k[0] = self.yp_k_minus[0] + self.ref_k[0] * Ts * math.sin(self.yp_k_minus[1] + self.ref_k[1] * Ts * 0.5)
+        self.ym_k[1] = self.yp_k_minus[1] + self.ref_k[1] * Ts
+        # self.ym_k[0] = self.yp_k_minus[0] + self.ref_k_minus[0] * Ts * math.sin(self.yp_k_minus[1] + self.ref_k_minus[1] * Ts * 0.5)
+        # self.ym_k[1] = self.yp_k_minus[1] + self.ref_k_minus[1] * Ts
 
-            rospy.set_param("~ym_k", yp_k)
-            rospy.set_param("~time_k", t_k)
-            rospy.set_param("~theta_hat_k", theta_hat_initialization_value)
-            rospy.set_param("~ref_k", np.asarray([v, omega]))
-            #rospy.set_param("~ref_k", [v, omega])
-            #publish u = [0 , 0] wheel cmd
+        e =  self.yp_k - self.ym_k
 
-            self.initialized = True
-            self.log("Initialized AC")
+        theta_hat_k_d = - self.gamma * e[1]
+        self.theta_hat_k = self.theta_hat_k + Ts * theta_hat_k_d
+        # self.theta_hat_k = self.theta_hat_k_minus + Ts * theta_hat_k_d
 
+        self.ref_k = np.asarray([car_cmd.v, car_cmd.omega])
 
-        else:
-            time_k_minus = rospy.get_param("~time_k")
-            ym_k_minus = rospy.get_param("~ym_k")
-            ref_k_minus = rospy.get_param("~ref_k")
-            theta_hat_k_minus = rospy.get_param("~theta_hat_k")
+        car_cmd_corrected.v = self.ref_k[0]
+        car_cmd_corrected.omega = self.ref_k[1] + self.theta_hat_k
 
-            yp_pres = rospy.get_param("~yp_lastPose")
-            yp_k = yp_pres[0:2]
-            t_k = yp_pres[2]
+        self.pub_corrected_car_cmd.publish(car_cmd_corrected)
 
-            #   1) Calculate Ts from the past two yp
-            Ts = t_k - t_k_minus
-            #from lane-control they calculate Ts as image_delay = image_delay_stamp.secs + image_delay_stamp.nsecs / 1e9
-            #self.log("Ts = %f" ,%()) #non mi ricordo
+        self.yp_k_minus = self.yp_k
+        self.t_k_minus = self.t_k
 
-            #   2) Predict current ym based on previus yp, reference and Ts
-            # Forward integration using second order method (Runge-Kutta), predicting present ym based on previous step v and omega
-            ym_k_predicted[0] = ym_k_minus[0] + ref_k_minus[0] * Ts * math.sin(ym_k_minus[1] + ref_k_minus[1] * Ts * 0.5)
-            ym_k_predicted[1] = ym_k_minus[1] + ref_k_minus[1] * Ts
-
-
-            #   3) Calculate e
-            # Having ym_pres_predicted and the lane pose yp_pres we can compute e=yp-ym of previous iteration
-            e =  yp_k - ym_k_predicted #check error definition yp-ym or opposite
-
-            # NOTE: for the one parameter only case, we can choose to use even only one of the two elements of
-            # the error e
-
-            #   4) Update the Adaptation law (now up to present time)
-            # To solve for theta_hat a simple first order Forward Euler method is used (ADAPTATION LAW)
-            theta_hat_k_d = - gamma * e[1]
-            theta_hat_k = theta_hat + Ts * theta_hat_k_d
-
-            self.loginfo("theta hat: %f" % theta_hat_k)
-
-            #   5) Retrieve reference v and omega
-            v = car_cmd.v
-            omega = car_cmd.omega
-
-            #   6) Compute corrected control command
-            # Compute the corrected command (CONTROL LAW)
-            '''
-            self.km =
-            slef.kp =
-            self.u = # TODO - need to write a nice matrix form first
-            rospy.set_param("~u", u)
-            '''
-            car_cmd_corrected.v = v
-            car_cmd_corrected.omega = omega + theta_hat_k
-
-            # Pubblish corrected car command
-            self.pub_corrected_car_cmd.publish(car_cmd_corrected)
-
-            # Set params for next function call
-            rospy.set_param("~ym_k", yp_pres)
-            rospy.set_param("~time_k", t_k)
-            rospy.set_param("~theta_hat_k", theta_hat)
-            rospy.set_param("~ref_k", np.asarray([v, omega]))
-
+        # self.theta_hat_k_minus = self.theta_hat_k
+        # self.ref_k_minus = self.ref_k
 
         # end_time = rospy.Time.now()
         # latency = end_time - start_time
