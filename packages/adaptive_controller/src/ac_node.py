4bf88b650c1804ed40fb8c762e86de6c9d886245
arsimone <arsimone@student.ethz.ch>
2019-12-17 18:46:53 +0100

    working project

--- packages/adaptive_controller/src/ac_node.py
+++ packages/adaptive_controller/src/ac_node.py
@@ -10,6 +10,7 @@ import math
 
 from duckietown import DTROS
 from duckietown_msgs.msg import Twist2DStamped, LanePose, WheelsCmdStamped, BoolStamped, FSMState, StopLineReading
+from sensor_msgs.msg import Joy
 
 from scipy.ndimage.interpolation import shift
 
@@ -28,23 +29,38 @@ class AdaptiveControllerNode(DTROS):
     Publisher:
         ~car_cmd (:obj:'Twist2DStamped'): The input signal as computed from the control law of
             our adaptive controller.
+        ~joystick_override (:obj:`duckietown_msgs/BoolStamped`): Boolean that is used to control whether
+           lane-following or joystick control is on
     """
 
     def __init__(self, node_name):
 
-        self.first = 0
+        self.first_iteration = True
+        self.stage = 0
 
         # Initialize the DTROS parent class
         super(AdaptiveControllerNode, self).__init__(node_name=node_name)
-
+        self.i = 0 
         # Get robot name
         self.veh_name = rospy.get_namespace().strip("/")
 
-        self.gamma = rospy.get_param("~gamma") #default 0.1
         self.error2use = rospy.get_param("~error2use")
-        if not self.error2use : self.log("Using error on d")
-        if self.error2use : self.log("Using error on phi")
+        self.trim_param_name = "/" + self.veh_name + "/kinematics_node/trim"
+        self.trim_actual = rospy.get_param(self.trim_param_name)
 
+        self.thresholds = [0.04, 0.035, 0.030]
+
+        if not self.error2use : 
+            self.log("Using error on d")
+            self.gammas = [5.0, 3.0, 2.0]
+            #self.gamma = rospy.set_param("~gamma",self.gammas[0]) 
+            self.gamma = self.gammas[self.stage]
+        if self.error2use : 
+            self.log("Using error on phi")
+            self.gammas = [0.8, 0.6, 0.4]
+            #self.gamma = rospy.set_param("~gamma",self.gammas[0]) 
+            self.gamma = self.gammas[self.stage]
+            
         self.lane_pose_k_minus = np.asarray([0.0, 0.0])
         self.t_lane_pose_k_minus = rospy.Time.now().to_sec()
         self.lane_pose_k = np.asarray([0.0, 0.0])
@@ -65,11 +81,12 @@ class AdaptiveControllerNode(DTROS):
         self.omega_max = rospy.get_param("/" + self.veh_name + "/lane_controller_node/omega_max")
 
         # Initialize past_theta_hats: this is gonna be used to keep track of of theta_hats' behavior
-        self.past_theta_hats = np.zeros(10)
+        self.past_theta_hats = np.zeros(50)
 
         # Publication
         self.pub_corrected_car_cmd = rospy.Publisher("lane_controller_node/car_cmd", Twist2DStamped, queue_size=1)
-
+        self.pub_stop_cmd = rospy.Publisher("joy", Joy, queue_size=1)
+        
         # Subscriptions, no tilde otherwise will sub to topic like ac_node/lane_pose which doesn't exist
 
         pose_sub = message_filters.Subscriber("lane_filter_node/lane_pose", LanePose)
@@ -77,18 +94,19 @@ class AdaptiveControllerNode(DTROS):
 
         ts = message_filters.TimeSynchronizer([pose_sub, cmd_sub], 10)
         ts.registerCallback(self.correctCommand)
+        self.display_param()
 
         self.log("Initialized")
 
     def correctCommand(self, poseMsg, car_cmd):
-
         # In here we want to do three things:
         #   1) Compute Ts from the past two yp
         #   2) Predict current ym based on previus yp, reference and Ts
         #   3) Calculate e
         #   4) Update the Adaptation law (now up to present time)
         #   5) Compute corrected control command
-
+        self.t_start = rospy.Time.now().to_sec()
+        #Syncronization between topics
         assert car_cmd.header.stamp == poseMsg.header.stamp
 
         self.log("==============================")
@@ -97,8 +115,9 @@ class AdaptiveControllerNode(DTROS):
 
         self.t_lane_pose_k = car_cmd.header.stamp.to_sec()
 
-        if not self.first:
-            self.first = 1
+        #Initialize at first iteration
+        if self.first_iteration:
+            self.first_iteration = False
             self.t_lane_pose_k_minus = car_cmd.header.stamp.to_sec()
             self.ac_rif_k_minus = np.asarray([car_cmd.v, car_cmd.omega])
             self.t_lane_pose_k_minus = self.t_lane_pose_k
@@ -116,16 +135,11 @@ class AdaptiveControllerNode(DTROS):
         # Init message
         car_cmd_corrected = Twist2DStamped()
 
-        # The error e is proportional on the sampling time Ts, so the multiplicative constant gamma that multiply
-        # e to obtain the correction of the inputs to the motors should be corrected by some factor of Ts
-        #gamma = self.gamma / Ts
-
-        # (2) : Predict current ym based on previus yp, reference and Ts
-
         #filter out TS too big or too small
         #do not update theta in curves omega accepted -1.0<omega< +1.0
         if (Ts >0.025) and (Ts < 0.25) and (car_cmd.omega < 1.5) and (car_cmd.omega > -1.5) :
 
+            # (2) : Predict current ym based on previus yp, reference and Ts
             self.lane_pose_k_predicted[0] = self.lane_pose_k_minus[0] + self.ac_rif_k_minus[0] * Ts * math.sin(self.lane_pose_k_minus[1] + self.ac_rif_k_minus[1] * Ts * 0.5)
             self.lane_pose_k_predicted[1] = self.lane_pose_k_minus[1] + self.ac_rif_k_minus[1] * Ts
 
@@ -142,18 +156,21 @@ class AdaptiveControllerNode(DTROS):
             
             delta_e = self.err_k -self.err_k_minus
             # Upper bounds for reasonable delta_e
-            ub = np.asarray([self.ac_rif_k[0]*Ts*2, self.omega_max*Ts*2])
+            upper_bound = np.asarray([self.ac_rif_k[0]*Ts*2, self.omega_max*Ts*2])
 
-            # We want to avoid updating theta_hat when:
-            #   1 - the returned lane pose is too far from previuos enstimate (probably unreliable)
-            #   2 - when in curves (recgnize curves based on angular speed)
-            cond_on_err  = (np.absolute(delta_e) < ub)
+            # We want to avoid updating theta_hat when the returned lane pose is too far from previuos estimate (probably unreliable)
+            cond_on_err  = (np.absolute(delta_e) < upper_bound)
             if (np.any(cond_on_err)) :
 
                 # Check for update on gamma
-                self.gamma = rospy.get_param("~gamma")
                 self.error2use = rospy.get_param("~error2use")
+                if not self.error2use : 
+                    self.gammas = [4.0, 3.0, 2.0]
+                if self.error2use : 
+                    self.gammas = [0.8, 0.6, 0.4]
 
+
+                self.gamma = self.gammas[self.stage]
                 # (4) : Update the Adaptation law
                 theta_hat_k_d = - self.gamma * self.err_k[self.error2use] #default 0, use error on d
                 self.theta_hat_k = self.theta_hat_k + Ts * theta_hat_k_d
@@ -161,10 +178,15 @@ class AdaptiveControllerNode(DTROS):
                 self.log("theta_hat_k : %f" % self.theta_hat_k)
                 self.past_theta_hats = shift(self.past_theta_hats, 1, cval=self.theta_hat_k)
 
-                trim_temp = + (np.mean(self.past_theta_hats)*np.asarray([0.1/0.46]))
-                self.log("difference used to actual trim : %f" % trim_temp)
+                self.trim_actual = rospy.get_param(self.trim_param_name)
+                trim_temp = self.trim_actual + (np.mean(self.past_theta_hats)*np.asarray([0.1/0.46]))
+                self.log("trim applied to system : %f" % trim_temp)
+                self.i = self.i+1   
             else:
-                self.log("theta not updated!")
+                self.log("theta not updated delta_e too big!")
+
+        else:
+            self.log("theta not updated conditions not satisfied!")
 
 	    # (5) : Compute corrected control command
         car_cmd_corrected.v = self.ac_rif_k[0]
@@ -177,17 +199,21 @@ class AdaptiveControllerNode(DTROS):
         # Publish corrected command
         car_cmd_corrected.header = car_cmd.header
         self.pub_corrected_car_cmd.publish(car_cmd_corrected)
-
-        # Shift buffer and add last theta_hat_k
-        # self.past_theta_hats = np.append(self.past_theta_hats[1:],np.asarray(self.theta_hat_k))
         
 
         # If theta hat is converging, then slowly reduce gamma
-        #TO_DO
+        #if self.i > 20:
+        #    self.onShutdown() 
 
-        # For similar reason as above, we want to increase gamma in case theta_hat start converging to a very
-        #   different value from before (for instance because of a bump):
-
+        span = np.max(self.past_theta_hats) - np.min(self.past_theta_hats) 
+        self.log("span : %f" % span)
+        gamma_decay_condition = span < self.thresholds[self.stage]
+        #self.past_theta_hats[-1] != 0.0 and self.i > 20 those conditions are to prevent stop before all the past_theta_hats buffer is full and i >20 is to have at least 20 iterations before changing gamma
+        if gamma_decay_condition and self.past_theta_hats[-1] != 0.0 and self.i > 50:
+            self.stage = self.stage + 1
+            self.i = 0 #set incremental variable to zero start count down again
+            if self.stage == 3:
+                self.onShutdown()
 
         # Update variables for next iteration
         self.lane_pose_k_minus = self.lane_pose_k
@@ -195,22 +221,32 @@ class AdaptiveControllerNode(DTROS):
         self.err_k_minus = self.err_k
         self.ac_rif_k_minus_2 = self.ac_rif_k_minus
         self.ac_rif_k_minus = self.ac_rif_k
+
+        self.t_end = rospy.Time.now().to_sec()
+        latency = self.t_end - self.t_start
+        self.log("latency : %f" % latency)
         
     def onShutdown(self):
         #Shutdown procedure.
+        #Gives back control to joystick stopping the car
+
+        self.joy_param('stop')
+
         #Save yaml file with new trim value.
-    
+        
         #INITIALIZATION FOR YAML FILE SAVE
         # Add the node parameters to the parameters dictionary and load their default values
-        
         self.parameters = {}
     
         # Set parameters using a robot-specific yaml file if such exists
         self.parameters = self.readParamFromFile()
     
         self.SaveCalibration() #create new calibration file
-        
+
+        rospy.set_param("lane_controller_node/ac_on" , False)
+        self.joy_param('stop')
         super(AdaptiveControllerNode, self).onShutdown()
+
     
     def SaveCalibration(self):
         """
@@ -222,10 +258,9 @@ class AdaptiveControllerNode(DTROS):
         v_bar_param_name = "/" + self.veh_name + "/lane_controller_node/v_bar"
         v_bar = rospy.get_param(v_bar_param_name)
     
-      	new_trim = - (np.asarray(self.theta_hat_k)*np.asarray(self.parameters['baseline'])*np.asarray(self.parameters['gain']))/(2.0*np.asarray(v_bar))
-        trim_param_name = "/" + self.veh_name + "/kinematics_node/trim"
+      	new_trim = - (np.mean(self.past_theta_hats)*np.asarray(self.parameters['baseline'])*np.asarray(self.parameters['gain']))/(2.0*np.asarray(v_bar))
         # Write to a yaml file
-        trim_used = rospy.get_param(trim_param_name)
+        trim_used = rospy.get_param(self.trim_param_name)
         new_trim = round(trim_used,4) - round(new_trim.astype(float),4) 
         if new_trim > 0.25 or new_trim < -0.25 :
         	new_trim = 0.0
@@ -250,10 +285,10 @@ class AdaptiveControllerNode(DTROS):
             outfile.write(yaml.dump(data, default_flow_style=False))
     
         # Printout
-        saved_details = "[gain: %s\t trim: %s\t baseline: %s\t radius: %s\t k: %s\t limit: %s\t omega_max: %s\t v_max: %s\t]" % \
-        (self.parameters['gain'], self.parameters['trim'], self.parameters['baseline'], self.parameters['radius'],
+        saved_details = "[gain: %s\n trim: %s\n baseline: %s\n radius: %s\n k: %s\n limit: %s\n omega_max: %s\n v_max: %s\n]" % \
+        (self.parameters['gain'], new_trim, self.parameters['baseline'], self.parameters['radius'],
          self.parameters['k'], self.parameters['limit'], self.parameters['omega_max'], self.parameters['v_max'])
-        self.log("Saved kinematic calibration to %s with values: %s" % (file_name, saved_details))
+        self.log("Saved kinematic calibration to %s with values:\n %s" % (file_name, saved_details))
     
     def readParamFromFile(self):
         """
@@ -292,7 +327,38 @@ class AdaptiveControllerNode(DTROS):
         cali_file = cali_file_folder + name + ".yaml"
         return cali_file
 
+    def joy_param(self, command):
+        if command == 'stop':
+            axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            buttons = [0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0]
+            stop_cmd_msg = Joy(header=None, axes=axes, buttons=buttons)
+            self.pub_stop_cmd.publish(stop_cmd_msg)
+        if command == 'start':
+            axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+            buttons = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0]
+            start_cmd_msg = Joy(header=None, axes=axes, buttons=buttons)
+            self.pub_stop_cmd.publish(start_cmd_msg)
 
+    def display_param(self):
+        self.parameters = {}
+        self.parameters = self.readParamFromFile()
+
+        data = {
+            "calibration_time": time.strftime("%Y-%m-%d-%H-%M-%S"),
+            "gain": self.parameters['gain'],
+            "trim": self.parameters['trim'],
+            "baseline": self.parameters['baseline'],
+            "radius": self.parameters['radius'],
+            "k": self.parameters['k'],
+            "limit": self.parameters['limit'],
+            "v_max": 1.0,
+            "omega_max": 8.0,
+        }
+        current_configuration = "[gain: %s\n trim: %s\n baseline: %s\n radius: %s\n k: %s\n limit: %s\n omega_max: %s\n v_max: %s\n]" % \
+        (self.parameters['gain'], self.parameters['trim'], self.parameters['baseline'], self.parameters['radius'],
+         self.parameters['k'], self.parameters['limit'], self.parameters['omega_max'], self.parameters['v_max'])
+        self.log("Initial kinematic parameters: \n%s" % (current_configuration))
+    
 
 if __name__ == '__main__':
     # Initialize the node
